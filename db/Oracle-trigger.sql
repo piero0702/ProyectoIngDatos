b56945179d2fbd871f8d3406f945ebaf924f8ce9
piero0702 <pieropizarrobasile@gmail.com>
2024-07-13 21:12:40 -0500

    trigger mejorado

--- db/Oracle-trigger.sql
+++ db/Oracle-trigger.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TRIGGER validar_stock
+CREATE OR REPLACE TRIGGER validar_y_actualizar_stock
 BEFORE INSERT ON pedidos_productos
 FOR EACH ROW
 DECLARE
@@ -7,6 +7,16 @@ BEGIN
     SELECT stock INTO v_stock FROM productos WHERE id = :NEW.producto_id;
     IF v_stock < 1 THEN
         RAISE_APPLICATION_ERROR(-20001, 'Stock insuficiente para el producto.');
+    ELSE
+        UPDATE productos
+        SET stock = stock - 1
+        WHERE id = :NEW.producto_id;
     END IF;
 END;
 /
+
+INSERT INTO pedidos_productos (pedido_id, producto_id, tipoProducto_id) VALUES (1, 3, 1);
+
+
+SELECT * FROM productos
+    
